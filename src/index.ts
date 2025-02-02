8bbb3a6b4fb9136b4c9e69553a243d89906ed99b
Francesco Nicolo <nicolo.francesco.fn@gmail.com>
2025-02-02 23:16:58 +0100

    fixed routes weapon

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use("/api/weapons", weaponRoute);
+app.use("/api/weapon", weaponRoute);
 
 app.get("/ping", (req, res) => {
   res.status(200).send({ prova: "ciao" });
